829f72bfb3839da5a04c7b6645d4b6ad71e40d48
francis-chris5 <50467171+francis-chris5@users.noreply.github.com>
2020-12-06 17:28:00 -0500

    Add files via upload

--- threeP.games/GUI/Components/Console.py
+++ threeP.games/GUI/Components/Console.py
@@ -10,26 +10,26 @@ import wx.py.shell
 import code
 
 class Console(wx.Panel):
-	def __init__(self, parent):
-		super().__init__(parent)
-		
-		shell = wx.py.shell.Shell(self, size=(700, 450))
-		
-	def getText(self):
-		return self.__name
-	
-	def setText(self, text):
-		self.__name = text
+    def __init__(self, parent):
+        super().__init__(parent)
+        
+        # I want to put a system console in this panel
+        
+    def getText(self):
+        return self.__name
+    
+    def setText(self, text):
+        self.__name = text
 
 
 
 class gui(wx.Frame):
-	def __init__(self, *args, **kwargs):
-		super().__init__(*args, **kwargs)
-		c = Console(self)
-		self.Show()
-		
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        c = Console(self)
+        self.Show()
+        
 if __name__ == "__main__":
-	app = wx.App()
-	gui(None, -1, "Tree Test")
-	app.MainLoop()
+    app = wx.App()
+    gui(None, -1, "Tree Test")
+    app.MainLoop()
--- threeP.games/GUI/Components/InspectorTab.py
+++ threeP.games/GUI/Components/InspectorTab.py
@@ -8,39 +8,39 @@ Created on Sat Dec  5 16:34:00 2020
 import wx
 
 class InspectorTab(wx.Panel):
-	def __init__(self, parent):
-		super().__init__(parent)
-		nameLabel = wx.StaticText(self, -1, "Name: ", pos=(10, 10))
-		self.__name = wx.TextCtrl(self, -1, size=(200, 20), pos=(10, 30))
-		positionLabel = wx.StaticText(self, -1, "Position", pos=(10, 60))
-		self.__x = wx.TextCtrl(self, -1, size=(30, 20), pos=(10, 80))
-		self.__y = wx.TextCtrl(self, -1, size=(30, 20), pos=(50, 80))
-		self.__y = wx.TextCtrl(self, -1, size=(30, 20), pos=(90, 80))
-		rotationLabel = wx.StaticText(self, -1, "Rotation", pos=(10, 110))
-		self.__h = wx.TextCtrl(self, -1, size=(30, 20), pos=(10, 130))
-		self.__p = wx.TextCtrl(self, -1, size=(30, 20), pos=(50, 130))
-		self.__r = wx.TextCtrl(self, -1, size=(30, 20), pos=(90, 130))
-		
-		
-		collisionArea = wx.StaticText(self, -1, "This Area is for collision stuff, My idea is have object images/animations/bounds preview here and drag out from image and get two comboboxes popup to choose collision and function--any thoughts/suggestions on this", pos=(400, 30), size=(200, 150))
-		scriptingArea = wx.StaticText(self, -1, "This Area is for managing scripts attached to this game object, probably simple combobox selection to start with at least", pos=(400, 220), size=(200, 150))
-		otherArea = wx.StaticText(self, -1, "Obviously this project is a long way from completion enough to even give it a version 1.x, these controls are coming, but for now it all has to be done in the scripting tab", pos=(20, 300), size=(200, 100))
-		
-	def getText(self):
-		return self.__name
-	
-	def setText(self, text):
-		self.__name = text
+    def __init__(self, parent):
+        super().__init__(parent)
+        nameLabel = wx.StaticText(self, -1, "Name: ", pos=(10, 10))
+        self.__name = wx.TextCtrl(self, -1, size=(200, 20), pos=(10, 30))
+        positionLabel = wx.StaticText(self, -1, "Position", pos=(10, 60))
+        self.__x = wx.TextCtrl(self, -1, size=(30, 20), pos=(10, 80))
+        self.__y = wx.TextCtrl(self, -1, size=(30, 20), pos=(50, 80))
+        self.__y = wx.TextCtrl(self, -1, size=(30, 20), pos=(90, 80))
+        rotationLabel = wx.StaticText(self, -1, "Rotation", pos=(10, 110))
+        self.__h = wx.TextCtrl(self, -1, size=(30, 20), pos=(10, 130))
+        self.__p = wx.TextCtrl(self, -1, size=(30, 20), pos=(50, 130))
+        self.__r = wx.TextCtrl(self, -1, size=(30, 20), pos=(90, 130))
+        
+        
+        collisionArea = wx.StaticText(self, -1, "This Area is for collision stuff, My idea is have object images/animations/bounds preview here and drag out from image and get two comboboxes popup to choose collision and function--any thoughts/suggestions on this", pos=(400, 30), size=(200, 150))
+        scriptingArea = wx.StaticText(self, -1, "This Area is for managing scripts attached to this game object, probably simple combobox selection to start with at least", pos=(400, 220), size=(200, 150))
+        otherArea = wx.StaticText(self, -1, "Obviously this project is a long way from completion enough to even give it a version 1.x, these controls are coming, but for now it all has to be done in the scripting tab", pos=(20, 300), size=(200, 100))
+        
+    def getText(self):
+        return self.__name
+    
+    def setText(self, text):
+        self.__name = text
 
 
 
 class gui(wx.Frame):
-	def __init__(self, *args, **kwargs):
-		super().__init__(*args, **kwargs)
-		p = InspectorTab(self)
-		self.Show()
-		
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        p = InspectorTab(self)
+        self.Show()
+        
 if __name__ == "__main__":
-	app = wx.App()
-	gui(None, -1, "Inspector Test", size=(700, 450))
-	app.MainLoop()
+    app = wx.App()
+    gui(None, -1, "Inspector Test", size=(700, 450))
+    app.MainLoop()
