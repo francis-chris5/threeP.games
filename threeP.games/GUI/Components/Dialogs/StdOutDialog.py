ed25bacda4447a01581ba5718a10688e9b69e984
francis-chris5 <50467171+francis-chris5@users.noreply.github.com>
2020-12-14 14:56:48 -0500

    Add files via upload

--- threeP.games/GUI/Components/Dialogs/StdOutDialog.py
+++ threeP.games/GUI/Components/Dialogs/StdOutDialog.py
@@ -11,6 +11,7 @@ sys.path.insert(1, "C:\\Users\\Chris\\Documents\\game dev in python\\threeP.game
 sys.path.insert(2, "C:\\Users\\Chris\\Documents\\game dev in python\\threeP.games\\GUI\\Components")
 import wx
 import interfaceStuff
+from datetime import datetime
 
 
 class StdOutDialog(wx.Dialog):
@@ -24,6 +25,8 @@ class StdOutDialog(wx.Dialog):
         self.__result = list(result)
         if self.__result[0] == b"":
             self.__result[0] = b"No messages on sys.stdout from this process"
+        if self.__result[1] == b"":
+            self.__result[1] = b"No messages on sys.stderr from this process"
         
             # good feedback
         self.__stdout = wx.StaticText(self, -1, label=self.__result[0].decode('ascii'), size=(280, 200), pos=(20, 20), style=wx.ST_NO_AUTORESIZE)
@@ -39,8 +42,21 @@ class StdOutDialog(wx.Dialog):
         
             
             # Clickers
+        self.__copy = wx.Button(self, wx.ID_OK, label="Copy", pos=(170, 235))
         self.__cancel = wx.Button(self, wx.ID_CANCEL, label="Close", pos=(280, 235))
-
+        
+        
+        
+        
+    def copyMessage(self):
+        filepath = interfaceStuff.location + "\\" + "STDOUT and STDERR Log.txt"
+        with open(filepath, "w") as toFile:
+            toFile.write("LOGGED AT: " + str(datetime.now()) + "\n\n")
+            toFile.write("\t\t--sys.stdout--\n")
+            toFile.write(self.__stdout.GetLabel() + "\n\n")
+            toFile.write("\t\t--sys.stderr--\n")
+            toFile.write(self.__stderr.GetLabel() + "\n")
+        return filepath
         
         
         
