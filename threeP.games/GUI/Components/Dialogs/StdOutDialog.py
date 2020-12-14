0b1bf7646c0943efa115fb11354705dd752af26d
francis-chris5 <50467171+francis-chris5@users.noreply.github.com>
2020-12-14 13:44:02 -0500

    Add files via upload

---
+++ threeP.games/GUI/Components/Dialogs/StdOutDialog.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 14 09:19:41 2020
+
+@author: Christopher S. Francis
+"""
+
+
+import sys
+sys.path.insert(1, "C:\\Users\\Chris\\Documents\\game dev in python\\threeP.games\\Interface")
+sys.path.insert(2, "C:\\Users\\Chris\\Documents\\game dev in python\\threeP.games\\GUI\\Components")
+import wx
+import interfaceStuff
+
+
+class StdOutDialog(wx.Dialog):
+    def __init__(self, parent, result=""):
+        super().__init__(parent)
+        self.SetTitle("sys.stdout")
+        self.SetSize((600, 320))
+        self.__icon = wx.Icon("images\\threep_logo.png")
+        self.SetIcon(self.__icon)
+        
+        self.__result = list(result)
+        if self.__result[0] == b"":
+            self.__result[0] = b"No messages on sys.stdout from this process"
+        
+            # good feedback
+        self.__stdout = wx.StaticText(self, -1, label=self.__result[0].decode('ascii'), size=(280, 200), pos=(20, 20), style=wx.ST_NO_AUTORESIZE)
+        self.__stdout.SetForegroundColour((50, 168, 82))
+        self.__stdout.Wrap(260)
+        
+            # bad feedback
+        self.__stderr = wx.StaticText(self, -1, label=self.__result[1].decode('ascii'), size=(280, 200), pos=(300, 20), style=wx.ST_NO_AUTORESIZE)
+        self.__stderr.SetForegroundColour((237, 57, 40))
+        self.__stderr.Wrap(260)
+        
+            
+        
+            
+            # Clickers
+        self.__cancel = wx.Button(self, wx.ID_CANCEL, label="Close", pos=(280, 235))
+
+        
+        
+        
